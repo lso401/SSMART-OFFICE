397ab23a7c3622719717d39c5e6a325ff1c5dff2
김세민 <2007ksm@naver.com>
2024-11-04 22:18:55 +0900

    FIX: #12 api-gateway 패키지 설정 수정
    Former-commit-id: cd0980fef669443470b5f8c972c4a16447eacb0c

---
+++ backend/api-gateway/src/main/kotlin/com/smartoffice/apigateway/jwt/JwtAuthenticationFilter.kt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6f1c6411a4ce8fa21bf2da93362328ced83d5c2c8ae34288eb2d02508c4d9d47
+size 1330
---
+++ backend/api-gateway/src/main/kotlin/com/smartoffice/apigateway/jwt/JwtTokenProvider.kt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f3ef3a08184770a23be9c25ad81cde15b92f03049b925dafb7af5f0ef7bab504
+size 1534
