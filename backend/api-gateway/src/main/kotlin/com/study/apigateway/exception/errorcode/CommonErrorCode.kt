8c8ab0da8df4d4edc0dd97fb50e3f67e3b7adaac
김세민 <2007ksm@naver.com>
2024-11-04 17:53:57 +0900

    CHORE: #12 api-gateway 설정 및 인증인가 코드 일부 이전
    Former-commit-id: a5f5059ac0940a1e8b812d25dd1379427f0a1575

---
+++ backend/api-gateway/src/main/kotlin/com/study/apigateway/exception/errorcode/CommonErrorCode.kt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5e8ddd234edb5cd0d0d3fc29e7f637bb47fc37f7cd425518b090d99f66b48998
+size 699
---
+++ backend/api-gateway/src/main/kotlin/com/study/apigateway/jwt/JwtAuthenticationFilter.kt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b8d700d2deef745288cfe914539a4c2afddbb6f47119411eb10f9d1ebfa80b45
+size 1324
