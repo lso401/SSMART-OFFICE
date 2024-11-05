5dece8aa2473eed9d2c17b69cf38f347a49d531d
김세민 <2007ksm@naver.com>
2024-11-05 10:45:30 +0900

    CHORE : #14 global 설정 값 추가
    Former-commit-id: 9567e133a3cf84e4031f235ede43f399be94f5f9

---
+++ backend/user-service/src/main/kotlin/com/ssmartofice/userservice/global/exception/RestApiException.kt
@@ -0,0 +1,10 @@
+package com.ssmartofice.userservice.global.exception
+
+import com.ssmartofice.userservice.global.exception.errorcode.ErrorCode
+
+class RestApiException(val errorCode: ErrorCode) : RuntimeException() {
+
+    companion object {
+        private const val serialVersionUID = 8747231388755467240L
+    }
+}
