55f5ad9b19e7b5b61c3c71be5fa05fb7baaa04f2
Lee soyeon <168151572+lso401@users.noreply.github.com>
2024-11-05 09:40:30 +0900

    FEAT : #9 header bar 구현
    Former-commit-id: 9bc3d1dcc50e8e88f6d008d3fcac3267e53eb118

--- frontend/src/pages/Message.jsx
+++ frontend/src/pages/Message.jsx
@@ -1,3 +1,11 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8cdc13cef8592a1f44c5b006e434462e4ca28bbca8630107d7a9622dbaadd365
-size 158
+const Message = () => {
+  return (
+    <div>
+      <h1>Message</h1>
+      <h2>Message</h2>
+      <h3>Message</h3>
+    </div>
+  );
+};
+
+export default Message;
--- frontend/src/pages/MyPage.jsx
+++ frontend/src/pages/MyPage.jsx
@@ -1,3 +1,8 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:ed6095bd0a8e4c64909cb7f47e441f7a30158099fc920a1bdc53fa00eb827ca6
-size 108
+const MyPage = () => {
+  return (
+    <div>
+      <h1>MyPage</h1>
+    </div>
+  );
+};
+export default MyPage;
