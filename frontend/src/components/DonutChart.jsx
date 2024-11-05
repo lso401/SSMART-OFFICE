5d7f3c1535a39ceae80b16f86f01bfaa4cd6d30b
wonbin <wonbin1301@naver.com>
2024-11-05 17:06:43 +0900

    FEAT : #10 인증/인가 및 페이지 구현
    - 좌석 현황 페이지 구현
    
    Signed-off-by: wonbin <wonbin1301@naver.com>
    
    Former-commit-id: 4409d785241b47e807bfa1199b2ee2ecc82693ba

--- frontend/src/components/DonutChart.jsx
+++ frontend/src/components/DonutChart.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:486563457a582a798de96d2059f90beb865345f9cd65822a3f55d8a018861cbb
-size 1703
+oid sha256:6e6e146f873270b19dcbf1fd42a7c67eeea7776bb6bfb21ee1ccffcd2052c1a2
+size 1595
--- frontend/src/components/SeatingByFloor.jsx
+++ frontend/src/components/SeatingByFloor.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5a418786eca2f34827f2cb4910741b47ac0bb17ab5f0a076ea0ef42a874aab88
-size 830
+oid sha256:99d20653a7453f9a9e2bfe884ab58868d1e45630a7bf1d052ff7e229b8867f7b
+size 1745
