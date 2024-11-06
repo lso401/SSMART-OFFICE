2cf37197beae6d3d6110cab69108197e9aa5bb00
이동복 <typoon0820@gmail.com>
2024-11-06 13:48:52 +0900

    FEAT: #20 discovery 서버 구현
    Former-commit-id: 36fdc371501cdb4db93d9dc161a1c36d75c54b21

--- backend/config-server/src/test/kotlin/org/example/configserver/ConfigServerApplicationTests.kt
+++ backend/config-server/src/test/kotlin/org/example/configserver/ConfigServerApplicationTests.kt
@@ -1,3 +1,13 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:82906ae8f0521a73946ad6a8782e58d17b378c2c42e3cb534ae2773a087297ab
-size 218
+package org.example.configserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
