24d2777041e02c62838c1c7f7258718078289dc1
Lee soyeon <168151572+lso401@users.noreply.github.com>
2024-11-06 17:24:47 +0900

    FEAT : #18 사내메시지 멤버 리스트 카드 구현
    Former-commit-id: a1a8e9fa6cc567ed0cfd5c3e221113f1e1364740

--- frontend/src/components/Message/MemberList.jsx
+++ frontend/src/components/Message/MemberList.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1c790804413c374cc9dfe696899eb11c149dfab4017b7bd2d3d938eaf9d964a8
-size 123
+oid sha256:1b009b6b167c4fc70ad15e40e4e2edae43480fdc7fad0b936e5703277d6de4c9
+size 1205
--- frontend/src/pages/Message.jsx
+++ frontend/src/pages/Message.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:66d58e65f6f45c5092c1f476b81b98020c6eb037a4dd638ff0a293e9134e15c8
-size 469
+oid sha256:6387706f44a9c4419abe0abd90f11e8d87a9b5ada37a03957502566a2d65e9f6
+size 1707
