0bc91d73ee45ee361beaea15c7dc85c1b2a8ae70
Ubuntu <ubuntu@ip-172-31-19-86.ap-northeast-2.compute.internal>
2024-11-06 10:07:35 +0000

    Feat: #3 Add Jenkins
    Former-commit-id: 0dcfc82a2e652cacd6ab17d0529f5dd1f3cca895

--- backend/user-service/src/main/kotlin/org/ssmartofice/userservice/user/domain/Role.kt
+++ backend/user-service/src/main/kotlin/org/ssmartofice/userservice/user/domain/Role.kt
@@ -1,3 +1,5 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e1c93cd3a56f678def9c3e23e4bac2d210357cbfd7d91b9998ad975350a0ea7f
-size 91
+package com.ssmartofice.userservice.user.domain
+
+enum class Role {
+    GUEST, USER, ADMIN
+}
