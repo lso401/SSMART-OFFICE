22142f6f3c0ee6808289684ab0b4447ddd370426
Lee soyeon <168151572+lso401@users.noreply.github.com>
2024-11-07 17:36:40 +0900

    FEAT: #18 사내메시지 구현_직원 리스트UI, 직원 카드UI, 1:1 메신저 UI 구현
    Former-commit-id: c23540160268b3a90b8f116961ea1c8de07dbe26

--- backend/api-gateway/src/main/kotlin/org/smartoffice/apigateway/ApiGatewayApplication.kt
+++ backend/api-gateway/src/main/kotlin/org/smartoffice/apigateway/ApiGatewayApplication.kt
@@ -1,11 +1,3 @@
-package org.smartoffice.apigateway
-
-import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
-
-@SpringBootApplication
-class ApiGatewayApplication
-
-fun main(args: Array<String>) {
-    runApplication<ApiGatewayApplication>(*args)
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:2d433f9b47900cbde7720281deb5eaa7c7ea2d05398a3851c41e8d839ea1f8a9
+size 287
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:669733c0b250b09c0019bfdc5812f5d2f6a5f1bb6187dad76167bfa6c95184f2
-size 352
+oid sha256:fa329da4ac5652474c0c30035ddebbdaf314e847e64d223591b6296909681fbe
+size 486
