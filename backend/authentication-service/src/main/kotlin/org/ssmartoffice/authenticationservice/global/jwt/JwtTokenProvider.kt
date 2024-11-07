42cfbbe11edd8f6c0ef60cddd490f2ea09b2cff3
김세민 <2007ksm@naver.com>
2024-11-08 02:25:24 +0900

    REFACTOR : #19 구글 로그인 엔드포인트 수정
    Former-commit-id: ebabb0c8887a8137cafdb43dda93b9efd2569202

--- backend/authentication-service/src/main/kotlin/org/ssmartoffice/authenticationservice/global/jwt/JwtTokenProvider.kt
+++ backend/authentication-service/src/main/kotlin/org/ssmartoffice/authenticationservice/global/jwt/JwtTokenProvider.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2610e96d22311c9eb2feddef3f85fbb4f32aef2188af61bea87357e19a2fc1c3
-size 4871
+oid sha256:f4382d528dd641fa7c63c917b8fdf53601a1120ab62d7d02ccd89c91dfa832be
+size 4278
--- backend/user-service/src/main/kotlin/org/ssmartoffice/userservice/global/config/SecurityConfig.kt
+++ backend/user-service/src/main/kotlin/org/ssmartoffice/userservice/global/config/SecurityConfig.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c8bee11824a667b8479041ec0b2d9d097b6c5e460be0f6db4f1b8d59c9723377
-size 2105
+oid sha256:d3bf3bc2da99f404e63601fd0449b43bf742db9e95af194495e65803cdebf673
+size 2083
