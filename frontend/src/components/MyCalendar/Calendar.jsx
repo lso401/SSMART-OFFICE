8f9e701f03f1c50801acf6c940afb7885d1c2aed
Lee soyeon <168151572+lso401@users.noreply.github.com>
2024-11-08 16:40:42 +0900

    FEAT : #17 캘린더 UI 수정
    Former-commit-id: 5dd57cc18f0e8f5080809a62ef0a1f2a40ad5b1d

--- frontend/src/components/MyCalendar/Calendar.jsx
+++ frontend/src/components/MyCalendar/Calendar.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2435720e2c6b127e060620ed6311cc6889b65c3d61d016bdfcde88183b0c5e04
-size 2352
+oid sha256:2b3968d853a14ec167c6250d1bc230c96fb0adde296c4558a80ae63f3c183238
+size 3400
