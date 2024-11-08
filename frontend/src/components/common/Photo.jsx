945ec8deebdbfe5c14037cd978d18542e4088f37
Park SangCheon <pswlove38@naver.com>
2024-11-08 09:50:15 +0900

    Feat: #3 Add Dockerfile
    Former-commit-id: 15c35fc5b3dd90d9c9737f2a9e3f1c680348cade

--- frontend/src/components/common/Photo.jsx
+++ frontend/src/components/common/Photo.jsx
@@ -1,3 +1,7 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a64010955621544f342674bff220524a124934a509de04b76f3ab24446f879cb
-size 98
+import React from "react";
+
+const Photo = () => {
+  return <div></div>;
+};
+
+export default Photo;
