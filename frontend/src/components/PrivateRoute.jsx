786942a2cca041086ecf568e99c0abd03042f144
wonbin <wonbin1301@naver.com>
2024-11-08 09:08:26 +0900

    FEAT: #10 [Frontend] 인증/인가 및 페이지 구현
    - oauth 구글 로그인
    - JWT 상태관리
    - privateRoute 구현(토큰이 없을 경우 로그인으로 이동 처리)
    
    Signed-off-by: wonbin <wonbin1301@naver.com>
    
    Former-commit-id: 0491b350bf6ea37e827e352113269643a4c3a580

---
+++ frontend/src/components/PrivateRoute.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ad16cf97b7dd2d1dd7766579977c4f8bdcbeb90ae788adc04d8a001bd998d21d
+size 334
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:76d8c711d2653e556f787d949c9662052fd2f82778ec37287b8372e9406bb11e
-size 231
+oid sha256:939cfbb59c1b860352bbfedc3d6871c5be6e502ba2ff0508d9b57d0a62e69af1
+size 138
