8e4160330df4ac11eb3bce5171b85e75177587ca
김세민 <2007ksm@naver.com>
2024-11-08 13:13:06 +0900

    REFACTOR : #19 인증 서버에 불필요한 객체 및 필터 제거
    Former-commit-id: a7498a678918e31daa7ed3b66d25be979d1c2e74

--- backend/authentication-service/src/main/kotlin/org/ssmartoffice/authenticationservice/domain/User.kt
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5f587db4847f159cf4d0706352f0ae3b0681b33014cf11e9896eb2603a03ebd2
-size 892
--- backend/authentication-service/src/main/kotlin/org/ssmartoffice/authenticationservice/infrastructure/UserEntity.kt
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:95853687d54941ecf763bd2476105a0e5d818d19f4dcd900ea2a771754fd0ea8
-size 1295
