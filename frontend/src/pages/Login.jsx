96485ca22d23ed9d8343a3fdf36fa2cca23014de
wonbin <wonbin1301@naver.com>
2024-11-08 14:46:24 +0900

    FEAT : #10 [Frontend] 인증/인가 및 페이지 구현
    - 로그인 페이지 css 수정
    - 좌석현황 페이지 css 수정
    
    Signed-off-by: wonbin <wonbin1301@naver.com>
    
    Former-commit-id: 624e8d169ab30b4b64279705472b6a62b824f09d

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:da80b566d89b09d0dbb5efff4331d9c496f30a569c00b3f3c6db76f664623735
-size 1887
+oid sha256:e5b9755f13a181d710d57eb8f582bd3b419809e0add1a42639a523a9e8925ae1
+size 1770
