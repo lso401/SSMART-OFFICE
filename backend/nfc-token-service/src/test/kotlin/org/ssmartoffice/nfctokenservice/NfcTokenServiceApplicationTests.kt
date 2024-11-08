219ecd5d23b93cf7903bfb225a1469dad651af44
이동복 <typoon0820@gmail.com>
2024-11-08 15:21:44 +0900

    FEAT: #24 nfc-token-service 개발
    Former-commit-id: 578782a89842d3b951119bb32a7f77808cf1bf25

---
+++ backend/nfc-token-service/src/test/kotlin/org/ssmartoffice/nfctokenservice/NfcTokenServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.ssmartoffice.nfctokenservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NfcTokenServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
