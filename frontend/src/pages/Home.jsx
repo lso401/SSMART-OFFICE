74495642780cc2105d64e1295fd2f60dd95ac001
Lee soyeon <168151572+lso401@users.noreply.github.com>
2024-11-08 16:40:42 +0900

    FEAT : #17 캘린더 UI 수정
    Former-commit-id: fdfc2a6af1dd26e7e0c3a9006affe7e9e93d6276

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0d33c2bf6eb24f4f1f3b990e251c211570702e3bfbc7175d5e1992c6bf0984b1
-size 1167
+oid sha256:8a26e4a696a65346eb105b5843362489e231603d78495ff58d83d5cedb53bdce
+size 2274
