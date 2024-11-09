110552ebbd200bcca99c70ae2d481dc537deec34
SangCheonP <pswlove38@naver.com>
2024-11-09 10:03:20 +0000

    Feat: #3 Test
    Former-commit-id: a2e8e4f3b153d310b529b3d21d9252403a630ca7

--- backend/auth_module/src/main/kotlin/org/example/auth_module/global/auth/Role.kt
+++ backend/auth_module/src/main/kotlin/org/example/auth_module/global/auth/Role.kt
@@ -1,5 +1,3 @@
-package org.example.auth_module.global.auth
-
-enum class Role {
-    GUEST, USER, ADMIN
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:5ccb59e32ddc64070e9920fe252eafda97c8d359433ae4d3f5130e4a15549f9c
+size 88
