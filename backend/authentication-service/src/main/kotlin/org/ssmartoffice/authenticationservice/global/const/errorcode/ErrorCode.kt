08574ed555f1c8a31823091274c5ce7f109c6c4f
Lee soyeon <168151572+lso401@users.noreply.github.com>
2024-11-11 17:13:15 +0900

    FEAT #22:사내현황페이지_UI구현
    Former-commit-id: 4f665bf2f5c8f3783bb734965d1f86ab8d21061b

--- backend/authentication-service/src/main/kotlin/org/ssmartoffice/authenticationservice/global/const/errorcode/ErrorCode.kt
+++ backend/authentication-service/src/main/kotlin/org/ssmartoffice/authenticationservice/global/const/errorcode/ErrorCode.kt
@@ -1,3 +1,10 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:badad502a2a640739cef94b2803a506e5ed5ffa65078af854fe6a88634e69888
-size 258
+package org.ssmartoffice.authenticationservice.global.const.errorcode
+
+import org.springframework.http.HttpStatus
+import java.io.Serializable
+
+interface ErrorCode : Serializable {
+    val httpStatus: HttpStatus
+    val message: String
+    val name: String
+}
