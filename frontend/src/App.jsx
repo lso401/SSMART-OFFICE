8e412e67a882006f5b2542a4d508f40aabf72c2c
wonbin <wonbin1301@naver.com>
2024-11-11 16:12:44 +0900

    FEAT: #10 [Frontend] 인증/인가 및 페이지 구현
    - 마이 페이지 모달 구현
    1. 비밀번호 수정
    2. 사진 변경
    3. 개인정보 수정
    
    Signed-off-by: wonbin <wonbin1301@naver.com>
    
    Former-commit-id: 62ae5fd9f5c5b318a9a66534b9dc80a3a3796fa6

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ababe52351b82afabd710f36ef1e47295916478cbb5af9b126227b5151ac2ec3
-size 770
+oid sha256:7378417f3c609744f75fe9b0d03c6bfad1931db80fa3778ff0519d2c746849b5
+size 836
--- frontend/src/components/MyPage/MyInfo.jsx
+++ frontend/src/components/MyPage/MyInfo.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ca4b46f0751ecf2bd7ec999e5279a6e47dbf290e8eaac6eaf3ec4999ff25ffbd
-size 1379
+oid sha256:3b445eb4e1544ad1ca54e501d6dfcd30fca59d327f5c78e7717d50687f0410ac
+size 2717
