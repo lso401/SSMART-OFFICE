e1828799834845dda20f3dd52cbeb75387c5e466
정원빈 <wonbin1301@naver.com>
2024-11-12 14:57:34 +0900

    Update frontend/src/pages/Login.jsx
    Former-commit-id: 94ca168791d9252f1fc6a7c380b84f757b1f7bc5

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0805c7691a56cf398fbafd3e4d4eed2c2655cf2b24dbb5fc7393e43048b559bf
-size 1850
+oid sha256:58add3f0a5ff37d03c68273b352cf3015a90793465de8183faff9f0e8f9c974a
+size 1851
