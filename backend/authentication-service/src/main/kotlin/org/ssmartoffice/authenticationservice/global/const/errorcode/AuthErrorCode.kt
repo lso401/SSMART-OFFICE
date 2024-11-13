a2fb396ba27d08285fe5cfb05b8c44ffe08ca235
김세민 <2007ksm@naver.com>
2024-11-13 17:48:54 +0900

    REFACTOR : InternalController 제거, 로그인 예외처리 추가
    Former-commit-id: 0311ae973ac8638f3aa9f3761e9c2408d11d16c0

--- backend/authentication-service/src/main/kotlin/org/ssmartoffice/authenticationservice/global/const/errorcode/AuthErrorCode.kt
+++ backend/authentication-service/src/main/kotlin/org/ssmartoffice/authenticationservice/global/const/errorcode/AuthErrorCode.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bffc2e737c0808cfa6e7e750b14217b072176e3fb4a620179c45dcda7546c012
-size 1796
+oid sha256:9013af161aed67e22586e03b0595fdc1710dfa4774167d421b1e7c9c98144de5
+size 1894
