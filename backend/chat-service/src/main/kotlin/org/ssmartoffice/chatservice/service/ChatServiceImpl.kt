06de44b107463f8d39e9bad2be389639f6e66d70
이동복 <typoon0820@gmail.com>
2024-11-13 14:28:11 +0900

    FEAT: #34 chat-service 구현
    Former-commit-id: 697735551fb1cf5fe7ad23a9008b88ffffe2ae8e

--- backend/chat-service/src/main/kotlin/org/ssmartoffice/chatservice/service/ChatServiceImpl.kt
+++ backend/chat-service/src/main/kotlin/org/ssmartoffice/chatservice/service/ChatServiceImpl.kt
@@ -1,3 +1,7 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:553fd278ace4846e2ce1b3d470c6435fc1412870fe6b0d021cb7ad9b1c01f82d
-size 127
+package org.ssmartoffice.chatservice.service
+
+import org.springframework.stereotype.Service
+
+@Service
+class ChatServiceImpl {
+}
--- backend/chat-service/src/main/kotlin/org/ssmartoffice/chatservice/service/port/ChatRepository.kt
+++ backend/chat-service/src/main/kotlin/org/ssmartoffice/chatservice/service/port/ChatRepository.kt
@@ -1,3 +1,4 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:01d7376f67ca80fa23d8352f9cb2d966fc946e8021bbf14ba6f693b32733dfcb
-size 80
+package org.ssmartoffice.chatservice.service.port
+
+interface ChatRepository  {
+}
