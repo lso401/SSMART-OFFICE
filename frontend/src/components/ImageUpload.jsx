4f45d8e27b6437fe8855b6b511f4826143ad24c3
wonbin <wonbin1301@naver.com>
2024-11-14 15:41:50 +0900

    FEAT : #33 [Frontend] API 연결
    Signed-off-by: wonbin <wonbin1301@naver.com>
    
    Former-commit-id: 7b1f63543012d063c6ca7abf31b6b8c0741ad8e1

--- frontend/src/components/ImageUpload.jsx
+++ frontend/src/components/ImageUpload.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f6f8b4845a42847180b961bf64813ea6ab62f0390c2aac3864faa8cd6304073
-size 963
+oid sha256:0b0a31de0f581a8cfdca4ec1de946145f4760c1a189d7134351a56c2e4d11f6e
+size 995
