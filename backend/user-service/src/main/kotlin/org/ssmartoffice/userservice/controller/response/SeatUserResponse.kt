7a9e75c3129bef6a0ca1c4152ec1230a5fe70d03
김세민 <2007ksm@naver.com>
2024-11-14 11:10:12 +0900

    REFACTOR : #35 dto model의존성 제거, dto 필드 수정
    - id -> userId
    - 이메일 업데이트 방지
    - 검증 로직 모델에서 서비스로 분리
    
    
    Former-commit-id: f8abc71025b385d0121896d33c220510a186c3e4

--- backend/user-service/src/main/kotlin/org/ssmartoffice/userservice/controller/response/SeatUserResponse.kt
+++ backend/user-service/src/main/kotlin/org/ssmartoffice/userservice/controller/response/SeatUserResponse.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f0a862aa21568c731b6ca178854be3794ae6318ee2239d64b31db770b15263cf
-size 516
+oid sha256:b20f1b2bde48a1ea6ca45b07449f372e133183a1936d85920067bb787aacb4ae
+size 188
