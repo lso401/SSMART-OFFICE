e09dec6ac0b562867c1a3b9166badda2102871b3
김세민 <2007ksm@naver.com>
2024-11-14 23:38:18 +0900

    FIX : #35 폰 정규 표현식 수정, 일정에 날짜 없던 버그 수정
    Former-commit-id: bb7a675cc1b68a93a19f359b3100c49a915153d8

--- backend/user-service/src/main/kotlin/org/ssmartoffice/userservice/controller/request/UserUpdateRequest.kt
+++ backend/user-service/src/main/kotlin/org/ssmartoffice/userservice/controller/request/UserUpdateRequest.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bc3f46b6a5c0c9abfcf297c782ff64853d5e3a5396d7c20f81373b1c2e4deec8
-size 541
+oid sha256:41406acc5211b7136cf5da1620ffa406ddef9c5d2fc7569b422618145e6a7cc4
+size 534
