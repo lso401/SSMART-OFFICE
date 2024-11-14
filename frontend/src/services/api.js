011068e623740601717de955d6f6dd9973c90804
wonbin <wonbin1301@naver.com>
2024-11-14 09:38:24 +0900

    FIX : #33 [Frontend] API 연결(authAPI 수정)
    Former-commit-id: e0e07f299b321b50ca10146e68f8bc6964937a37

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:06741527529c1f040cbee3cf26ad69930aa22d72c9780294df6463932c76d337
-size 1382
+oid sha256:c27ddff961e602dcdc9d66108900f016e6e9c347a7ab6e4a89ae6b158b4717bc
+size 1389
