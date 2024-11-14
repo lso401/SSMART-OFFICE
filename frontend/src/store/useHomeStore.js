c889ff14c9e7b76d3658a400499e68d2dcd212f2
wonbin <wonbin1301@naver.com>
2024-11-14 17:53:55 +0900

    FIX: #33 [Frontend] API 연결
    Former-commit-id: ef4855e1005fffcabe22ef4fb4cb892d49e9e0a3

--- frontend/src/store/useHomeStore.js
+++ frontend/src/store/useHomeStore.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:640acc73778fedda0f1662e04f5913593a815c6edcc66ee3b4b5a4f965da77be
-size 2152
+oid sha256:1b4aec459f884d310767307629269930cedaaf3fad0c1094389c7a9727980556
+size 2151
