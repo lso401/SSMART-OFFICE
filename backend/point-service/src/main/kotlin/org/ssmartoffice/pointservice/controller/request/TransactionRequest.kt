51d59737262fe6556b896cf9b63c23d766b201ec
김세민 <2007ksm@naver.com>
2024-11-15 17:45:35 +0900

    FEAT : #35 복지 포인트 사용
    Former-commit-id: b80857a26320fb99d2b4e3095272b1970a1d6322

---
+++ backend/point-service/src/main/kotlin/org/ssmartoffice/pointservice/controller/request/TransactionRequest.kt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:49f2a7a56fbbcd64109070fff85b3c03effcf0d54478378f2ea31f4f2a6f218e
+size 992
--- backend/point-service/src/main/kotlin/org/ssmartoffice/pointservice/service/port/PointHistoryRepository.kt
+++ backend/point-service/src/main/kotlin/org/ssmartoffice/pointservice/service/port/PointHistoryRepository.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc73ad52c3e9a4ebc8ea17d1bd2dac95413c07988059bbfb4fe1c114ea83ef29
-size 547
+oid sha256:f3d187ccdf9d01147c2ada648596132f21c302ee5f86e33225dca4f2cfcd2864
+size 588
