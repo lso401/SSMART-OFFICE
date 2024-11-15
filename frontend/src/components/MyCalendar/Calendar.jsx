c4b06ba2d3c523229960680525c798e06c71dff0
Lee soyeon <168151572+lso401@users.noreply.github.com>
2024-11-15 21:32:13 +0900

    FIX: 예외처리 추가
    Former-commit-id: 9a50c792fd63a60610d638c147f1ec70dd6de80f

--- frontend/src/components/MyCalendar/Calendar.jsx
+++ frontend/src/components/MyCalendar/Calendar.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d3b8d12d144a2d9796e6c88813f2944920effe91a2b51501ca4deca0c302375
-size 3839
+oid sha256:bc1baf8c261f90169042f9acde36ca97811e391718eed38c29ddb14209834484
+size 3855
