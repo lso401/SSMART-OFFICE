01eb794decf1848e5005b0fb892e504c3c725aa0
김세민 <2007ksm@naver.com>
2024-11-15 13:25:37 +0900

    FEAT : #35 좌석 로직 수정 및 예외처리 추가
    Former-commit-id: d4a3e15c170d3776b4178e55bc93046d23e1b51d

--- backend/seat-service/src/main/kotlin/org/ssmartoffice/seatservice/controller/port/SeatService.kt
+++ backend/seat-service/src/main/kotlin/org/ssmartoffice/seatservice/controller/port/SeatService.kt
@@ -1,12 +1,13 @@
 package org.ssmartoffice.seatservice.controller.port
 
-import org.ssmartoffice.seatservice.controller.request.SeatUpdateRequest
 import org.ssmartoffice.seatservice.domain.Seat
+import org.ssmartoffice.seatservice.domain.SeatStatus
 import org.ssmartoffice.seatservice.domain.User
 
 interface SeatService {
     fun getSeatsByFloor(floor: Int): List<Seat>
     fun getUsersAtSeats(seats: List<Seat>): List<User>
-    fun changeSeatStatus(id: Long, seatUpdateRequest: SeatUpdateRequest): Seat
-    fun getUserAtSeat(seat: Seat): User?
+    fun getSeatStatus(seatId: Long): Seat
+    fun getUserInfo(userId: Long): User?
+    fun changeSeatStatus(seat: Seat, requestUser: User?, requestStatus: SeatStatus)
 }
