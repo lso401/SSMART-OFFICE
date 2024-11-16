cecf343ad5d142007f3b686ac723df08ef6f81b6
이동복 <typoon0820@gmail.com>
2024-11-16 13:21:34 +0900

    FEAT: #38 채팅 기능 구현
    Former-commit-id: 36533c3fe828db9a8f41328a4c9c8d54757e13af

--- backend/chat-service/src/main/kotlin/org/ssmartoffice/chatservice/service/port/ChatRepository.kt
+++ backend/chat-service/src/main/kotlin/org/ssmartoffice/chatservice/service/port/ChatRepository.kt
@@ -1,4 +1,3 @@
-package org.ssmartoffice.chatservice.service.port
-
-interface ChatRepository  {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:569e88f6f06e05649f7039c5ce90be2a612b785b429252d556cb5a54815ec2fe
+size 505
