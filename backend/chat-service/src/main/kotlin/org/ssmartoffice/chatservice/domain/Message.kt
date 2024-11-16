57d153d063e63ac63767a22c00c9e89a81f98e59
이동복 <typoon0820@gmail.com>
2024-11-16 13:21:34 +0900

    FEAT: #38 채팅 기능 구현
    Former-commit-id: f1ecd5b7779672a333228a5a9632116a0f7dd047

--- backend/chat-service/src/main/kotlin/org/ssmartoffice/chatservice/domain/Message.kt
+++ backend/chat-service/src/main/kotlin/org/ssmartoffice/chatservice/domain/Message.kt
@@ -1,4 +1,14 @@
 package org.ssmartoffice.chatservice.domain
 
-class Message {
-}
+import java.time.LocalDateTime
+
+class Message(
+    val id :Long? = null,
+    val userId :Long,
+    val chatroom :ChatRoom,
+    val type :MessageType,
+    val content :String,
+    val deleted :Boolean = false,
+    val createdAt :LocalDateTime = LocalDateTime.now(),
+    val deletedAt :LocalDateTime = LocalDateTime.now()
+)
---
+++ backend/chat-service/src/main/kotlin/org/ssmartoffice/chatservice/domain/UserChatRoom.kt
@@ -0,0 +1,6 @@
+package org.ssmartoffice.chatservice.domain
+
+class UserChatRoom(
+    val id: Long? = null,
+    val userId: Long?,
+    val chatroomId: Long?)
