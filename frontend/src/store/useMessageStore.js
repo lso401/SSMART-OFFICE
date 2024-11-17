302b5e48b2af0c869da4b526a2c835656d4d03b0
정원빈 <wonbin1301@naver.com>
2024-11-17 14:42:11 +0900

    login 수정
    Former-commit-id: adf769970655dac83f7920757e2dd24c6c1bf4ca

--- frontend/src/store/useMessageStore.js
+++ frontend/src/store/useMessageStore.js
@@ -1,3 +1,8 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f1f233f00fd90f4992dd26f14999fd052597635a6ab181476da6ec19119d28a9
-size 182
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+const useHomeStore = create(
+  persist((set) => ({
+    // 채팅방 최신순 전체 조회
+  }))
+);
