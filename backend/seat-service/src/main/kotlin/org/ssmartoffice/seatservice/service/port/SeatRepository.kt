cce698fe66bc5e56567a76e982a8bba4699900bc
Lee soyeon <168151572+lso401@users.noreply.github.com>
2024-11-18 03:28:31 +0900

    FIX #41: 메시지 비동기 오류 수정
    Former-commit-id: e0479a2d6a2421fbce5e940efe36310f4199b9db

--- backend/seat-service/src/main/kotlin/org/ssmartoffice/seatservice/service/port/SeatRepository.kt
+++ backend/seat-service/src/main/kotlin/org/ssmartoffice/seatservice/service/port/SeatRepository.kt
@@ -1,9 +1,3 @@
-package org.ssmartoffice.seatservice.service.port
-
-import org.springframework.stereotype.Repository
-import org.ssmartoffice.seatservice.domain.Seat
-
-@Repository
-interface SeatRepository {
-    fun findAllByFloor(floor: Int): List<Seat>
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:7804ea60d897cb13287928251409cba55e1ad9e1f65e63afa0a67d4b3894a268
+size 236
