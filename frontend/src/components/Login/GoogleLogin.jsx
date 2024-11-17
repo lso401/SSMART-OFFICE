62b788e6833eaab081efbd8d93600c92c715b283
wonbin <wonbin1301@naver.com>
2024-11-17 13:37:14 +0900

    FEAT: #40 유효성 검사 및 CSS 수정(로그인 경로 수정)
    Former-commit-id: dfd48ce3ce9142f44627664deeedf267696950d3

--- frontend/src/components/Login/GoogleLogin.jsx
+++ frontend/src/components/Login/GoogleLogin.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:010669e8daa93b557a1e43e7cb6578ba1160a3048e1a1d113caa27659f8c39b8
+oid sha256:adbbe1ec7f61a4f9e0857c8aa21d1018352aed1c78fa1d23c7ff949ce0902f8f
 size 1884
