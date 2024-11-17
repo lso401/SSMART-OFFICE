449388a9cec5f7b3adb11701e7f3b31a4f0c157c
김세민 <2007ksm@naver.com>
2024-11-17 11:20:06 +0900

    FIX : #35 포인트 잔액 없을 때 처리
    Former-commit-id: 0843cecdac29769dcf68bf6fcfea6f16702d8660

--- backend/point-service/src/main/kotlin/org/ssmartoffice/pointservice/service/PointServiceImpl.kt
+++ backend/point-service/src/main/kotlin/org/ssmartoffice/pointservice/service/PointServiceImpl.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8c2a7e80c9c96533f93bad692726453ab65224518372db83abab006d8b20f6e9
-size 1379
+oid sha256:adeac5353de68899929a74d3b7ccc8620371a8d6b61dbcbe1da0c50243ef2e15
+size 1398
