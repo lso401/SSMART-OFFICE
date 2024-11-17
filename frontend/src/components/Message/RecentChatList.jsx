61279e69b4aed5afafc22779879cb3ff42a2cba2
Lee soyeon <168151572+lso401@users.noreply.github.com>
2024-11-18 00:48:58 +0900

    FEAT #37_채팅 CSS 수정
    Former-commit-id: fbd03a246741ec634a1dff85789a272c18dd1417

--- frontend/src/components/Message/RecentChatList.jsx
+++ frontend/src/components/Message/RecentChatList.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:043566805f83dc277df2c93bfaf085ac626e8b2fcf901b314016276da51e7a03
-size 2235
+oid sha256:4a5e0fd23e3c6c5199a84e4e03d86658fbfcd9aff686f1739d8d2fbb9a21fe64
+size 2341
