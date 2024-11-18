576da5c5cf2542b6f13c4df4193cfc213b66a8fa
wonbin <wonbin1301@naver.com>
2024-11-18 10:30:25 +0900

    FIX : 채팅 스크롤
    Former-commit-id: ef0f0ea29664b72062d450ef65dac3dcc9e9d46b

--- frontend/src/components/Message/Chat.jsx
+++ frontend/src/components/Message/Chat.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1c4df4a44badff8128a4a19278a603764152fff244eee0ebe9f3bbbf0f1d76fb
-size 3449
+oid sha256:4ac923dbd3cf29b5b304a88aa01f28034873780937aead6800014f6fb621c3b6
+size 3479
