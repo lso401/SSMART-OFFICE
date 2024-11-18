ece0eabcb45d0e54e771559099100203b5fee81c
이소연 <april_401@naver.com>
2024-11-18 11:53:14 +0900

    Update frontend/src/services/messageApi.js
    Former-commit-id: b97cbac49f62eacfc42144c5963855dd44f346b9

--- frontend/src/services/messageApi.js
+++ frontend/src/services/messageApi.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ee3b284c3a1f6f85ac95690bca232c9c458541de706a800141878d1b631bcc9c
-size 5362
+oid sha256:a925b63840f2074f95745cf30dd349d26b39e4127bfc5772c7ff259f339293c7
+size 5363
